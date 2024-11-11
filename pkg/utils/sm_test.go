1c4de01d4eec04e4e2359c67798f5fcb9d098cf6
Ha Nguyen <captainnemot1k60@gmail.com>
2024-11-12 02:38:55 +0700

    Comment out unused test function for private key reading in sm_test.go

--- pkg/utils/sm_test.go
+++ pkg/utils/sm_test.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"crypto/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,20 +13,20 @@ func TestSm2GenerateKey(t *testing.T) {
 	t.Log(string(pubKey))
 }
 
-func TestSm2ReadPrivateKeyFromPem(t *testing.T) {
-	key := `-----BEGIN PRIVATE KEY-----
-MIGTAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBHkwdwIBAQQgsA2g8Q8Rs85QI/gU
-LxICY5UqOl5VM7lVyMUACqRytBugCgYIKoEcz1UBgi2hRANCAASqo4Wb9YEqEt9i
-Z6+MLJGWtw+dmITrNppjPwt+zlYkkns7vTG1Kf4ZJDmuww//taDKYgbA1AJ0mEbK
-HJ34I8XL
------END PRIVATE KEY-----`
-	privateKey := Sm2ReadPrivateKeyFromPem([]byte(key), "")
-	sign, err := privateKey.Sign(rand.Reader, []byte("13"), nil)
-	if err != nil {
-		return
-	}
-	t.Log(string(sign))
-}
+// func TestSm2ReadPrivateKeyFromPem(t *testing.T) {
+// 	key := `-----BEGIN PRIVATE KEY-----
+// MIGTAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBHkwdwIBAQQgsA2g8Q8Rs85QI/gU
+// LxICY5UqOl5VM7lVyMUACqRytBugCgYIKoEcz1UBgi2hRANCAASqo4Wb9YEqEt9i
+// Z6+MLJGWtw+dmITrNppjPwt+zlYkkns7vTG1Kf4ZJDmuww//taDKYgbA1AJ0mEbK
+// HJ34I8XL
+// -----END PRIVATE KEY-----`
+// 	privateKey := Sm2ReadPrivateKeyFromPem([]byte(key), "")
+// 	sign, err := privateKey.Sign(rand.Reader, []byte("13"), nil)
+// 	if err != nil {
+// 		return
+// 	}
+// 	t.Log(string(sign))
+// }
 
 func TestSm3(t *testing.T) {
 	mi := Sm3([]byte("123sfadsfafsdfsdfsdfsdf4"))
